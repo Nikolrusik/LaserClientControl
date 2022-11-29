ccc999a7e532683421a603b0f130d4bf05d1f388
unknown <nikolrusik@gmail.com>
2022-11-29 22:30:00 +0300

    Added apps in installed_list, added user model

--- apps/user/models.py
+++ apps/user/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import PermissionsMixin, UserManager
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(PermissionsMixin, AbstractBaseUser):
+    firstname = models.CharField(_("firstname"), max_length=150, blank=True, null=True)
+    lastname = models.CharField(_("lastname"), max_length=150, blank=True, null=True)
+    email = models.CharField(_("Email"), unique=True, max_length=250)
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_("Designates whether the user can log into this admin site."),
+    )
+    is_active = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. \
+    Unselect this instead of deleting accounts."
+        ),
+    )
+    date_joined = models.DateTimeField(_("date joined"), auto_now_add=True)
+    objects = UserManager()
+    EMAIL_FIELD = "email"
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["email"]
