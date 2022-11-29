3a4be43a10f7ce7cf9e260c4e9af5cc575bfc405
unknown <nikolrusik@gmail.com>
2022-11-29 22:57:28 +0300

    Added model Sessions

--- apps/laser/models.py
+++ apps/laser/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+CHOICES_SESSION_STATUS = (
+    ("ACTIVE", "Активна"),
+    ("CLOSED", "Закрыта"),
+    ("NOT_COMED", "Не пришёл"),
+)
+
+
+class Clients:
+    firstname = models.CharField(_("First name"), max_length=150)
+    lastname = models.CharField(_("Last name"), max_length=150)
+
+
+class Tatto:
+    pass
+
+
+class Sessions:
+    client = models.ForeignKey(Clients)
+    tatto_list = models.ManyToManyField(Tatto)
+    datetime_session = models.DateTimeField(_("Date and time session"),
+                                            editable=True)
+    status = models.CharField(_("Status"),
+                              choices=CHOICES_SESSION_STATUS,
+                              default="ACTIVE")
+
+
+class Photos:
+    pass
