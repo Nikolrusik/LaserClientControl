21c70207cd553e1ae6d8dc9cffd536dc77b19ca7
unknown <nikolrusik@gmail.com>
2023-01-21 22:10:06 +0300

    Start creating frontend structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  connstructor(props) {
+    super(props);
+    this.state = {
+      clients: [],
+      sessions: [],
+      token: "",
+    };
+  }
+
+  get_headers() {
+    let headers = {
+      "Content-Type": "application/json",
+    };
+    if (this.is_authenticated()) {
+      headers["Autorization"] = "Token" + this.state.token;
+    }
+  }
+
+  is_authenticated() {
+    return this.state.token != "";
+  }
+
+  get_token(username, password) {
+    axios
+      .post("http://127.0.0.1:8000/api-token-auth/", {
+        username: username,
+        password: password,
+      })
+      .then((response) => {
+        console.log(response.data);
+        this.set_token(response.data["token"]);
+      })
+      .catch((error) => alert("Неверный логин или пароль"));
+  }
+
+  load_data() {
+    const headers = this.get_headers();
+  }
+
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<div></div>} />
+            <Route path="/new_session/new_client" element={<div></div>} />
+            <Route path="/new_session/active_client" element={<div></div>} />
+            <Route path="/sessions" element={<div></div>} />
+            <Route path="/not_comed" element={<div></div>} />
+            <Route path="/all_clients" element={<div></div>} />
+            <Route path="/birthdays" element={<div></div>} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
